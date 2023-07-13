0579c64f63f933e8712890a1193907a6be683cd6
Benjamin Phillips <benjamingt537@gmail.com>
2023-07-12 20:44:51 -0400

    got the dropdown menu working

--- src/app/components/Dropdown.jsx
+++ src/app/components/Dropdown.jsx
@@ -1,61 +1,76 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { useAnimate, stagger, motion } from "framer-motion";
 import Image from 'next/image';
-import { motion } from 'framer-motion';
 
 import Arrow from "@/app/assets/arrow.svg"
 
+function useMenuAnimation(isOpen) {
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animate(".arrow", { rotate: isOpen ? 90 : 0 }, { duration: 0.2 });
+
+        animate("ul",
+            {
+                clipPath: isOpen
+                    ? "inset(-25% -25% -25% -25% round 10px)"
+                    : "inset(10% 50% 90% 50% round 10px)"
+            },
+            {
+                type: "spring",
+                bounce: 0,
+                duration: 0.5
+            }
+        );
+
+        animate(
+            "li",
+            isOpen
+                ? { opacity: 1, scale: 1, y: 0 }
+                : { opacity: 0, scale: 0.5, y: 2 },
+            {
+                type: "spring",
+                damping: 14,
+                bounce: 10,
+                stiffness: 150,
+                delay: isOpen ? stagger(0.1, { startDelay: 0 }) : 0
+            }
+        );
+    }, [isOpen]);
+
+    return scope;
+}
+
 export default function Dropdown() {
-    const [dropdownState, setDropdownState] = useState(false)
-
-    let items = ["1", "2", "3", "4", "5"]
-
-    const variants = {
-        dropdownContainer: {
-            initial: { opacity: 0 },
-            animate: { opacity: 1, transition: { staggerChildren: 0.5 }},
-        },
-        dropdownItem: {
-            initial: { opacity: 0 },
-            animate: { opacity: 1 }
-        }
-    }
+    const [isOpen, setIsOpen] = useState(true);
+    const scope = useMenuAnimation(isOpen);
 
     return (
-        <section id='dropdown' className=' select-none h-screen bg-gradient-to-br from-violet-400 to-sky-400 grid place-items-center snap-center'>
-            <div className='w-96 h-96 rounded-md overflow-hidden'>
-                <motion.div 
-                    className='px-8 py-5 bg-white rounded-md cursor-pointer flex flex-row justify-between' 
-                    onClick={() => setDropdownState(prev => !prev)}
-                    whileTap={{ scale: 0.98, transition: { type: "spring", stiffness: 250 }}}
+        <section className="h-screen snap-center bg-gradient-to-br from-violet-400 to-sky-400 grid place-items-center">
+            <nav className="menu" ref={scope}>
+                <motion.button
+                    whileTap={{ scale: 0.97 }}
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="flex flex-row justify-between w-96 px-8 py-5 bg-white rounded-md shadow-md"
                 >
-                    <p className='text-xl font-semibold'>Dropdown</p>
-                    <Image 
+                    <p className="font-semibold">Dropdown</p>
+                    <Image
                         src={Arrow}
                         width={20}
                         height={20}
-                        className={`${dropdownState ? "rotate-90" : ""} transition-all`}
+                        className="arrow"
                     />
-                </motion.div>
-                <motion.ul 
-                    className='flex flex-col gap-2 mt-5'
-                    variants={variants.dropdownContainer}
-                    initial="initial"
-                    animate="animate"
-                >
-                    {dropdownState && items.map(e => (
-                        <motion.li 
-                            className='px-3 py-2 w-full text-xl font-semibold bg-white rounded-md'
-                            variants={variants.dropdownItem}
-                            initial="initial"
-                            animate="animate"
-                        >
-                            {e}
-                        </motion.li>
-                    ))}
-                </motion.ul>
-            </div>
+                </motion.button>
+                <ul className="flex flex-col gap-2 mt-2">
+                    <li className="shadow-md px-6 py-3 bg-white rounded-md font-semibold">Item 1 </li>
+                    <li className="shadow-md px-6 py-3 bg-white rounded-md font-semibold">Item 2 </li>
+                    <li className="shadow-md px-6 py-3 bg-white rounded-md font-semibold">Item 3 </li>
+                    <li className="shadow-md px-6 py-3 bg-white rounded-md font-semibold">Item 4 </li>
+                    <li className="shadow-md px-6 py-3 bg-white rounded-md font-semibold">Item 5 </li>
+                </ul>
+            </nav>
         </section>
-    )
+    );
 }
