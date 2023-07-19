b267e5f953ae3ebd74a6cef564a7c45a2dde590e
Benjamin Phillips <benjamingt537@gmail.com>
2023-07-19 16:56:33 -0400

    made the animations cooler

--- src/app/components/Clickers.jsx
+++ src/app/components/Clickers.jsx
@@ -22,7 +22,7 @@ function Clicker(props) {
 
     return (
         <motion.div 
-            className='w-4 h-4 bg-white rounded-full absolute overflow-hidden' 
+            className='w-4 h-4 bg-white rounded-md absolute overflow-hidden shadow-md' 
             style={{ transform: `translate(${pos.x}px, ${pos.y}px)` }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -34,16 +34,17 @@ function Clicker(props) {
 
 export default function Clickers() {
     const [amount, setAmount] = useState(0)
-    const [val1, setVal1] = useState(5)
-    const [val2, setVal2] = useState(0.5)
+    const [val1, setVal1] = useState(2)
+    const [val2, setVal2] = useState(1)
 
     useEffect(() => {
-        let prevAmount = 0
         let interval = 0
 
         const amountInterval = setInterval(() => {
-            prevAmount = 0
-            setAmount(Math.round((Math.sin(interval / 100) + 1) * 100))
+            let prevAmount = 0
+            setVal1((Math.sin(interval / 500) + 2))
+            // setVal2((Math.sin(interval / 500) + 2))
+            setAmount(Math.round((Math.sin(interval / 250) + 1) * 150))
             interval++
         }, 0);
 
@@ -52,11 +53,11 @@ export default function Clickers() {
 
     return (
         <section id='clickers' className='grid place-items-center snap-center overflow-hidden min-h-screen bg-gradient-to-br from-sky-400 to-purple-500'>
-            <div className='absolute top-0 left-0'>
-                <input type="range" min={0} max={1000} value={amount} onChange={e => setAmount(e.target.valueAsNumber)} className='w-full' />
-                <input type="range" min={0} max={5} value={val1} onChange={e => setVal1(e.target.valueAsNumber)} className='w-full' />
-                <input type="range" min={0} max={5} value={val2} onChange={e => setVal2(e.target.valueAsNumber)} className='w-full' />
-            </div>
+            {/* <div className='absolute top-0 left-0'>
+                <input type="range" min={0} max={1000} value={amount} step={1} onChange={e => setAmount(e.target.valueAsNumber)} className='w-full' />
+                <input type="range" min={0} max={5} value={val1} step={0.1} onChange={e => setVal1(e.target.valueAsNumber)} className='w-full' />
+                <input type="range" min={0} max={5} value={val2} step={0.1} onChange={e => setVal2(e.target.valueAsNumber)} className='w-full' />
+            </div> */}
             <div className='relative'>
                 {[...Array(amount)].map((e, i) => {
                     return (
